308c7c6f8c8a5a5379dedb48dfa11a29fbd794a2
AshotUltraLabs <a.simonyan.stdev@gmail.com>
2022-02-12 01:09:03 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import ComingSoon from "./components/ComingSoon/ComingSoon"
+
+function App() {
+  return <ComingSoon />
+}
+
+export default App
---
+++ src/components/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,54 @@
+import {ComingSoonStyle} from "./ComingSoon.style"
+import ComingLG from "../../assets/coming-soon/coming-soon.svg"
+import ComingMD from "../../assets/coming-soon/coming-soon-md.svg"
+import ComingSM from "../../assets/coming-soon/coming-soon-sm.svg"
+import ComingXS from "../../assets/coming-soon/coming-soon-xs.svg"
+import Logo from "../../assets/logo.svg"
+import {Icon} from "../UIKit/Icons/Icon"
+
+const ComingSoon = () => {
+    return (
+        <ComingSoonStyle>
+            <header>
+                <div className="logo">
+                    <img src={Logo} alt="Racing Social Club"/>
+                </div>
+            </header>
+            <section>
+                <picture>
+                    <source srcSet={ComingXS}
+                            media="(max-width: 576px) and (min-width: 360px)"/>
+                    <source srcSet={ComingSM}
+                            media="(max-width: 991px) and (min-width: 576px)"/>
+                    <source srcSet={ComingMD}
+                            media="(max-width: 1199px) and (min-width: 992px)"/>
+                    <source srcSet={ComingLG}
+                            media="(min-width:1200px)"/>
+                    <img src={ComingLG} alt="Coming Soon"/>
+                </picture>
+                {/*<img src={ComingImg} alt="Coming Soon"/>*/}
+            </section>
+            <footer>
+                <ul>
+                    <li>
+                        <a href="">
+                            <Icon name="fb"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <Icon name="instagram"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <Icon name="twitter"/>
+                        </a>
+                    </li>
+                </ul>
+            </footer>
+        </ComingSoonStyle>
+    )
+}
+
+export default ComingSoon
