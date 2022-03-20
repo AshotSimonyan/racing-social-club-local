f59fbac5f1e7044b718c586b077a57b5a2fb6777
AshotUltraLabs <a.simonyan.stdev@gmail.com>
2022-03-20 21:52:13 +0400

    Update RoadMap.jsx

--- src/components/RoadMap/RoadMap.jsx
+++ src/components/RoadMap/RoadMap.jsx
@@ -196,7 +196,7 @@ const RoadMap = forwardRef(({}, ref) => {
           <p className="text">
             The RSC fund is an initiative started by RSC, in order to create
             more attention for the financial hurdle that exists in racing. A lot
-            of talented races struggle with this during their career. We want to
+            of talented racers struggle with this during their career. We want to
             create attention, raise money and give money from the project back
             to the racers with this fund.
           </p>
