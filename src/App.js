aa4db6096f798393838a2ed13ff3198ac9c26c87
AshotSimonyan <a.simonyan.stdev@gmail.com>
2022-03-30 14:42:15 +0400

    change app file

--- src/App.js
+++ src/App.js
@@ -1,101 +1,116 @@
-import { useEffect, useState, useRef, useCallback } from "react"
+import { useEffect, useState, useRef, useCallback, useMemo } from "react"
 import About from "./components/About/About"
 import { useOnScreen } from "./hooks/useOnScreen"
-import Loader from "./components/Loader/Loader"
+import Loader from "./components/Loader/LoaderCopy"
 import { useLockedBody } from "./hooks/useLockedBody"
 import FAQ from "./components/FAQ/FAQ"
 import Team from "./components/Team/Team"
+import HorizontalScroll from "react-scroll-horizontal"
 import NavBar from "./components/Navbar/NavBar"
 import RoadMap from "./components/RoadMap/RoadMap"
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [progress, setProgress] = useState(5)
-  const [, setLocked] = useLockedBody(true)
-  const parentRef = useRef(null)
-  const faqRef = useRef(null)
-  const aboutRef = useRef(null)
-  const roadMapRef = useRef(null)
-  const teamRef = useRef(null)
+    const [loaded, setLoaded] = useState(false)
+    const [progress, setProgress] = useState(5)
+    const [animValues, setAnimValues] = useState(0)
+    const [touchDevice, setTouchDevice] = useState(false)
+    const parentRef = useRef(null)
+    const faqRef = useRef(null)
+    const aboutRef = useRef(null)
+    const teamRef = useRef(null)
+    const roadMapRef = useRef(null)
 
-  const aboutScreen = useOnScreen(aboutRef)
+    const aboutScreen = useOnScreen(aboutRef)
+    // const [, setLocked] = useLockedBody(true)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false)
-      setLocked(false)
-    }, 3500)
-  })
+    useEffect(() => {
+        window.history.scrollRestoration = "manual"
+    }, [])
 
-  const refMapping = {
-    "#about": aboutRef,
-    "#roadmap": roadMapRef,
-    "#faq": faqRef,
-    "#team": teamRef,
-  }
-
-  const handleWheel = useCallback(e => {
-    const left = parentRef.current.scrollLeft - e.nativeEvent.wheelDelta
-    parentRef.current.scrollTo({
-      left: left,
-    })
-    let progress =
-      ((left + parentRef.current.clientWidth) / parentRef.current.scrollWidth) *
-      100
-    if (left <= 0) {
-      progress = 5
+    const refMapping = {
+        "#about": aboutRef,
+        "#roadMap": roadMapRef,
+        "#faq": faqRef,
+        "#team": teamRef,
     }
-    setProgress(progress)
-  }, [])
 
-  const handleLinkClick = useCallback(to => {
-    const left = refMapping[to]?.current.offsetLeft
-    parentRef.current.scrollTo({
-      left: left,
-    })
-    setProgress(
-      ((left + parentRef.current.clientWidth) / parentRef.current.scrollWidth) *
-        100
+    useEffect(() => {
+        if (
+            typeof window !== "undefined" &&
+            window?.matchMedia("(pointer: coarse)").matches
+        ) {
+            setTouchDevice(true)
+        }
+    }, [])
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setLoading(false)
+    //         setLocked(false)
+    //     }, 3500)
+    // }, [])
+
+    const calculateProgress = useCallback(offsetLeft => {
+        const parent = document.querySelector(".parent >div")
+        const pageWidth = parseInt(window.getComputedStyle(parent).width)
+        let percent = Math.round(
+            ((offsetLeft + parentRef.current.hScrollParent.clientWidth) / pageWidth) *
+            100
+        )
+        if (percent < 5) {
+            percent = 5
+        }
+        return percent
+    }, [])
+
+    const handleLinkClick = to => {
+        const offsetLeft = parseInt(refMapping[to]?.current.offsetLeft)
+        const animValues = Math.abs(parentRef.current?.state.animValues)
+        if (touchDevice) {
+            refMapping[to]?.current.scrollIntoView({
+                inline: "start",
+            })
+        } else {
+            setAnimValues(animValues - offsetLeft)
+            setProgress(calculateProgress(offsetLeft))
+        }
+    }
+
+    const handleScroll = () => {
+        const animValues = Math.abs(parentRef.current?.state?.animValues)
+        setProgress(calculateProgress(animValues))
+    }
+
+    const handleLoad = () => {
+        setLoaded(true)
+    }
+
+    return (
+        <main>
+            <Loader onLoad={handleLoad} />
+            <div className="main-wrapper" onWheel={handleScroll}>
+                <HorizontalScroll
+                    reverseScroll
+                    className="parent"
+                    config={{ stiffness: 50 }}
+                    ref={parentRef}
+                    animValues={animValues}
+                >
+                    <About ref={aboutRef} onScreen={aboutScreen && loaded} />
+                    <RoadMap ref={roadMapRef} />
+                    <Team ref={teamRef} />
+                    <FAQ ref={faqRef} />
+                </HorizontalScroll>
+            </div>
+
+            <NavBar
+                onLinkClick={handleLinkClick}
+                progress={progress}
+                onScroll={handleScroll}
+                touchDevice={touchDevice}
+            />
+        </main>
     )
-  }, [])
-
-  const handleScroll = useCallback(() => {
-    requestAnimationFrame(() => {
-      const left = parentRef.current.scrollLeft
-      let progress =
-        ((left + parentRef.current.clientWidth) /
-          parentRef.current.scrollWidth) *
-        100
-      if (left <= 0) {
-        progress = 5
-      }
-      setProgress(progress)
-    })
-  }, [])
-
-  return (
-    <main>
-      <Loader className={loading ? "" : "hide-loader"} />
-      <div className="main-wrapper" onWheel={handleWheel}>
-        <div
-          className="horizontal-scroll"
-          ref={parentRef}
-          onScroll={handleScroll}
-        >
-          <About ref={aboutRef} onScreen={aboutScreen && !loading} />
-          <RoadMap ref={roadMapRef} />
-          <Team ref={teamRef} />
-          <FAQ ref={faqRef} />
-        </div>
-      </div>
-
-      <NavBar
-        onLinkClick={handleLinkClick}
-        progress={progress}
-        onScroll={handleScroll}
-      />
-    </main>
-  )
 }
 
 export default App
